a4ff468e5f1d4ca40e2a62a1890642c038fa946a
Andrew <andrewtclarkson@gmail.com>
2022-04-16 11:35:31 -0400

    rearranged code into a function, data outputs in browser

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from nltk.tokenize import word_tokenize, blankline_tokenize
 from nltk.corpus import stopwords
 # punction will be used to filter out numerous word/characters
 import re 
-punctuation = re.compile(r'[•%-.?!,:;()$`|0-9+*’™\']')
+punctuation = re.compile(r'[•%-.?!,:;()$`|0-9+*’™]')
 
 # This allows formatted objects in prints, and to write json
 import pprint
@@ -60,6 +60,91 @@ def get_pdf_file_content(path_to_pdf):
     return text
 
 
+pdf_entry = os.listdir(f'{UPLOAD_FOLDER}')
+
+
+def pdf_entry_returner():
+    for entry in pdf_entry:
+        return entry
+
+
+# dumped all of this into a function to call from flask route
+def analyze_data(path_to_pdf):
+    # Samples worked!!! - Instructions in README for how to create a pdf of your own in Google Docs
+    # path_to_pdf = "./assets/JobRec.pdf"
+    # path_to_pdf = "./assets/LinkedInSoftEng.pdf"
+    # path_to_pdf = "./assets/BakeryPackager.pdf"
+    # path_to_pdf = f"{UPLOAD_FOLDER}/{pdf_entry_returner()}"
+    # path_to_pdf = upload_file()
+    # path_to_pdf = upload_file()[1]
+    # print(path_to_pdf)
+
+    # Create a variable that contains the value of the pdf-turned-to-string
+    pdf_text = get_pdf_file_content(path_to_pdf)
+    # pdf_text = upload_file()
+
+    # Tokenize is covered in the NLTK tutorial. I forget what it does, but in general it splits up every word into a list item/ element that is a string.
+    pdf_tokens = word_tokenize(pdf_text)
+
+    # For each string element in the list, if any indexes fit the regex words, remove/don't include that characters (bullet points, %, ,, ...);
+    post_punctuation = []
+    for words in pdf_tokens:
+        word = punctuation.sub("", words)
+        # Don't keep it if there aren't any letters in it at all
+        if len(word)>0:
+            post_punctuation.append(word)
+
+    # Each word in the job description is aligned with an appropriate POS (parts of speech).
+    pos_jobrec = []
+    for token in post_punctuation:
+        pos_jobrec.append(nltk.pos_tag([token]))
+
+    # Instead of a list containing nested lists of tuples, unpack so that one list contains every tuple but with no surrounding individual lists.
+    tuple_extractor = []
+    for list_tuple in pos_jobrec:
+        tuple_extractor.append(list_tuple[0])
+
+    # Reverse tuples so POS appear first, almost like a K:V pair
+    def Reverse(tuples):
+        new_tup = tuples[::-1]
+        return new_tup
+    tuples_POS_first = []
+    for tuple in tuple_extractor:
+        tuples_POS_first.append(Reverse(tuple))
+
+    # Create a list of POS elements then make it unique w/ set()
+    unique_POS_list = []
+    for POS in tuples_POS_first:
+        unique_POS_list.append(POS[0])
+    unique_POS_list = list(set(unique_POS_list))
+
+    # Now all unique POS keys are stored in a dictionary/object
+    accordion_dict = {}
+    for POS in unique_POS_list:
+        accordion_dict[POS] = []
+
+    # This is a dictionary of POS lists that should be cleaned
+    for words in tuples_POS_first:
+        accordion_dict[words[0]] += [words[1]]
+
+    # for each list in the dict, lowercase each str in that list
+    for key, value in accordion_dict.items():
+        for i in range(len(value)):
+            value[i] = value[i].lower()
+
+    # Remove duplicate elements in each list
+    filtered_accordion = {}
+    for key, value in accordion_dict.items():
+        filtered_accordion[key] = list(set(value))
+
+    # pprint.pprint(filtered_accordion)
+
+    out_file = open("accordion.json", "w")
+    json.dump(filtered_accordion, out_file, indent = 6)
+    out_file.close()
+    return filtered_accordion
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -104,7 +189,17 @@ def upload_file():
 # Displays the pdf on screen
 @app.route('/uploads/<name>')
 def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], name)
+    # this is the entire pdf as a string
+    pdf_string = get_pdf_file_content(file_path)
+
+    analyzed_data = analyze_data(file_path)
+
+
+    return analyzed_data
+
+    # this is the download path
+    # return send_from_directory(app.config["UPLOAD_FOLDER"], name)
     # this is where you would work with the file. Instead of the current return, you could go with something like
     # with open(app.config["UPLOAD_FOLDER"], name) as file:
         # put pdf operations here
@@ -114,86 +209,7 @@ def download_file(name):
 # this code block typically goes at the very end of your file
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run('host='0.0.0.0, port=port)
     app.run(debug=True) #this will make it restart every time you save - super helpful
 
 # Uploading Flask Files CODE END
-
-pdf_entry = os.listdir(f'{UPLOAD_FOLDER}')
-def pdf_entry_returner():
-    for entry in pdf_entry:
-        return entry
-
-
-# Samples worked!!! - Instructions in README for how to create a pdf of your own in Google Docs
-# path_to_pdf = "./assets/JobRec.pdf"
-# path_to_pdf = "./assets/LinkedInSoftEng.pdf"
-# path_to_pdf = "./assets/BakeryPackager.pdf"
-path_to_pdf = f"{UPLOAD_FOLDER}/{pdf_entry_returner()}"
-# path_to_pdf = upload_file()
-# path_to_pdf = upload_file()[1]
-# print(path_to_pdf)
-
-# Create a variable that contains the value of the pdf-turned-to-string
-pdf_text = get_pdf_file_content(path_to_pdf)
-# pdf_text = upload_file()
-
-# Tokenize is covered in the NLTK tutorial. I forget what it does, but in general it splits up every word into a list item/ element that is a string.
-pdf_tokens = word_tokenize(pdf_text)
-
-# For each string element in the list, if any indexes fit the regex words, remove/don't include that characters (bullet points, %, ,, ...);
-post_punctuation = []
-for words in pdf_tokens:
-    word = punctuation.sub("", words)
-    # Don't keep it if there aren't any letters in it at all
-    if len(word)>0:
-        post_punctuation.append(word)
-
-# Each word in the job description is aligned with an appropriate POS (parts of speech).
-pos_jobrec = []
-for token in post_punctuation:
-    pos_jobrec.append(nltk.pos_tag([token]))
-
-# Instead of a list containing nested lists of tuples, unpack so that one list contains every tuple but with no surrounding individual lists.
-tuple_extractor = []
-for list_tuple in pos_jobrec:
-    tuple_extractor.append(list_tuple[0])
-
-# Reverse tuples so POS appear first, almost like a K:V pair
-def Reverse(tuples):
-    new_tup = tuples[::-1]
-    return new_tup      
-tuples_POS_first = []
-for tuple in tuple_extractor:
-    tuples_POS_first.append(Reverse(tuple))
-
-# Create a list of POS elements then make it unique w/ set()
-unique_POS_list = []
-for POS in tuples_POS_first:
-    unique_POS_list.append(POS[0])
-unique_POS_list = list(set(unique_POS_list))
-
-# Now all unique POS keys are stored in a dictionary/object
-accordion_dict = {}
-for POS in unique_POS_list:
-    accordion_dict[POS] = []
-
-# This is a dictionary of POS lists that should be cleaned
-for words in tuples_POS_first:
-    accordion_dict[words[0]] += [words[1]]
-
-# for each list in the dict, lowercase each str in that list
-for key, value in accordion_dict.items():
-    for i in range(len(value)):
-        value[i] = value[i].lower()
-
-# Remove duplicate elements in each list
-filtered_accordion = {}
-for key, value in accordion_dict.items():
-    filtered_accordion[key] = list(set(value))
-
-pprint.pprint(filtered_accordion)
-
-out_file = open("accordion.json", "w")
-json.dump(filtered_accordion, out_file, indent = 6)
-out_file.close()
