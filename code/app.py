a5affea2e182b6de106be1dcc4d4b81fbbc8f6b0
Pythonidaer <pythonidaer@gmail.com>
2022-04-28 14:00:42 -0400

    'help'

--- code/app.py
+++ code/app.py
@@ -173,4 +173,4 @@ def analyze(name):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     # app.run('host='0.0.0.0, port=port)
-    app.run(debug=True) #this will make it restart every time you save - super helpful
+    app.run(debug=True)
